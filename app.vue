a9db9a27a09e5c6cd0dbf715ed878243e5d5acb2
许圣峰 <kricsleo@163.com>
2024-01-05 14:12:05 +0800

    chore: init

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const msg = 'HI'
+</script>
+
+<template>
+  <main>
+    <h1 class="text-center">{{ msg }}</h1>
+  </main>
+</template>
+
+<style>
+html {
+  background: Canvas;
+  color: CanvasText;
+  color-scheme: light dark;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  modules: [
+    '@unocss/nuxt',
+  ],
+})
